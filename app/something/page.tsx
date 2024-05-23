ac1fc5244fc7db790be74ce642f902ae5f4e9d00
Maxwell Lawrence Jann <maxwellljann@gmail.com>
2024-05-23 03:41:27 -0500

    breckie

--- app/something/page.tsx
+++ app/something/page.tsx
@@ -9,5 +9,6 @@ export default function Page(){
         <iframe width="560" height="315" src="https://www.youtube.com/embed/sQRsVN2EFBU?si=h4UZW7z3JbEhECqW" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         < Link href={'/datareader' } > Data </Link >
         </>
+        
     )
 }
